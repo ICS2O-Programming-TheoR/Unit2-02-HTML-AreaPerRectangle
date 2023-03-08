4bc662fee247e367fb9e033e1964dd2ee0b80f59
TheoRafuse <124588617+TheoRafuse@users.noreply.github.com>
2023-03-08 15:52:28 +0000

    Updated index.html, style.css, and script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Theo Rafuse All rights reserved
+// Created by: Theo Rafuse
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('add-math').innerHTML = '46m + 28m = ' + (46 + 28) + "m"
+  document.getElementById('multiply-math').innerHTML = '46m * 28m = ' + (46 * 28) + "m<sup>2</sup>"
+}
